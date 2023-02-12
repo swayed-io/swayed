2b5f7ee7dfd70ae603ecafd07375a1a3d0993d9c
Elhafdaoui <zakaria.elhafdaoui@edu.uiz.ac.ma>
2023-02-12 11:56:54 +0100

    some changes in navbar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
+import GetStartedToday from "../components/earners/GetStartedToday";
+import EarnersHero from "../components/earners/EarnersHero";
+import ShareYourStudy from "../components/earners/ShareYourStudy";
+import EarnersMonthly from "../components/earners/EarnersMonthly";
+import Ourobsession from "../components/earners/Ourobsession";
+import Navbar from "../components/home/Navbar";
+import Sidebar from "../components/home/Sidebar";
+import Footer from "../components/home/Footer";
+import Faq from "../components/earners/Faq";
 
-const HomePage = () => {
-  return <div id="cool">HomePage</div>;
+const Earners = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <>
+      <Navbar setSidebarOpen={setSidebarOpen} />
+      <Sidebar setSidebarOpen={setSidebarOpen} isSidebarOpen={isSidebarOpen} />
+      <EarnersHero />
+      <div className="  ">
+        <GetStartedToday />
+      </div>{" "}
+      <ShareYourStudy />
+      <EarnersMonthly />
+      <Ourobsession />
+      <Faq />
+      <Footer />
+    </>
+  );
 };
 
-export default HomePage;
+export default Earners;

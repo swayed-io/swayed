41cec7412c2594b94d43e3e4ec01b37953d0b908
swayed-io <122157551+swayed-io@users.noreply.github.com>
2023-02-25 18:52:59 +0600

    stand alone true

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: {
+    standalone: true
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.

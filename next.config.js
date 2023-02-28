63ee4fb2226f6f1696d1ac28ae80664e80093c34
mirzashowvik <showvikmirza@gmail.com>
2023-02-28 12:11:14 +0600

    uptodate

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-  output: 'standalone',
+  distDir: 'build',
+  // output: 'standalone',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.

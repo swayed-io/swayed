36b0c24609b5f9b83ce8526ea7d314df8f958496
aliibnemasud <mdmasudrony@gmail.com>
2023-04-10 00:42:39 +0600

    google analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -52,6 +52,19 @@ function MyApp({ Component, pageProps }) {
                 }}
               ></script>
 
+              {/* <!-- Google tag (gtag.js) --> */}
+              <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=G-XNLDENHMZX"
+              ></script>
+              <script>
+                {`window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-XNLDENHMZX')`}
+              </script>
+
               <script async src="//static.getclicky.com/101383763.js"></script>
             </Head>
 
